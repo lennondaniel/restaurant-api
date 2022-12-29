7aa81cedd2e5467a9d7343422a65907850fe5e39
Lennon Daniel <lennon.v90@gmail.com>
2022-12-29 01:28:37 -0300

    add menu folder

--- src/entities/menu/menu.spec.ts
+++ src/entities/menu/menu.spec.ts
@@ -1,20 +1,21 @@
 import {expect, test} from "vitest";
 import {Category, Menu} from "./menu";
-import {Dish} from "./dish";
+import {Dish} from "../dish/dish";
 import * as crypto from "crypto";
 
 test('create a new menu option', () => {
     const dish = new Dish({
-        id: crypto.randomUUID(),
         name: 'Salada',
         image: 'foto-salada.jpg',
         description: 'Salada com tomate, alface e frango',
         price: 30.00
     });
+
     const menu = new Menu({
-        dish_id: 'Salada',
+        dish_id: dish.id,
         category: Category.APPETIZER,
     });
 
     expect(menu).toBeInstanceOf(Menu);
+    expect(dish).toBeInstanceOf(Dish);
 })
