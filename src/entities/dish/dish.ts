0c1198ecef8addb027dc8055b2eca937a1b430f3
Lennon Daniel <lennon.v90@gmail.com>
2022-12-30 17:35:52 -0300

    add propertie status and deleted_at

--- src/entities/dish/dish.ts
+++ src/entities/dish/dish.ts
@@ -5,7 +5,9 @@ export interface DishProps {
     name: string;
     image: string;
     description: string;
-    price: number
+    price: number;
+    status?: boolean;
+    deleted_at?: Date;
 }
 
 export class Dish {
@@ -13,11 +15,16 @@ export class Dish {
     public name: string;
     public image: string;
     public description: string;
-    public price: number
+    public price: number;
+    public status: boolean;
+    public deleted_at: Date;
     constructor(dish: DishProps) {
         Object.assign(this, dish);
         if(!dish.id){
             this.id = crypto.randomUUID();
+        }
+        if(!dish.status) {
+            this.status = true;
         }
 
     }
