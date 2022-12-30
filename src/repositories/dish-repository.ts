aa55afa5807ceac3c887bbf0a5a3f36d5790a958
Lennon Daniel <lennon.v90@gmail.com>
2022-12-30 17:37:02 -0300

    add respositorie function show, update, update status and delete

--- src/repositories/dish-repository.ts
+++ src/repositories/dish-repository.ts
@@ -1,6 +1,10 @@
 import {Dish} from "../entities/dish/dish";
 
 export interface DishRepository {
-    create(dish: Dish): Promise<void>
-    findOverlapping(name: string): Promise<Dish | null>
+    create(dish: Dish): Promise<void>;
+    findOverlapping(name: string, id?: string): Promise<Dish | null>;
+    show(id: string): Promise<Dish>;
+    update(id: string, dish: Dish): Promise<Dish>;
+    updateStatus(id: string): Promise<Dish>;
+    delete(id: string): Promise<Dish>;
 }
--- src/repositories/in-memory/dish-in-memory-repository.ts
+++ src/repositories/in-memory/dish-in-memory-repository.ts
@@ -7,15 +7,55 @@ export class DishInMemoryRepository implements DishRepository {
     create(dish: Dish): Promise<void> {
         this.dishes.push(dish);
     }
-    findOverlapping(name: string): Promise<Dish | null> {
+    findOverlapping(name: string, id: string = null): Promise<Dish | null> {
         return new Promise((resolve) => {
-            const overlappingDish = this.dishes.find(dish => dish.name === name);
+            const overlappingDish = this.dishes.find(dish => {
+                if(id){
+                    return dish.name === name && dish.id !== id
+                }
+
+                return dish.name === name
+            });
 
             if(!overlappingDish) {
                  resolve(null);
             }
 
             resolve(overlappingDish);
-        })
+        });
+    }
+
+    show(id: string): Promise<Dish> {
+        return new Promise((resolve) => {
+            const dish = this.dishes.find(dish => dish.id === id);
+            resolve(dish);
+        });
+    }
+
+    update(id: string, dish: Dish): Promise<Dish> {
+        return new Promise((resolve) => {
+            const dishIndex = this.dishes.findIndex(dish => dish.id === id);
+            this.dishes[dishIndex].name = dish.name;
+            this.dishes[dishIndex].image = dish.image;
+            this.dishes[dishIndex].description = dish.description;
+            this.dishes[dishIndex].price = dish.price;
+            resolve(this.dishes[dishIndex]);
+        });
+    }
+
+    updateStatus(id: string): Promise<Dish> {
+        return new Promise((resolve) => {
+            const dishIndex = this.dishes.findIndex(dish => dish.id === id);
+            this.dishes[dishIndex].status = !this.dishes[dishIndex].status;
+            resolve(this.dishes[dishIndex]);
+        });
+    }
+
+    delete(id: string): Promise<Dish> {
+        return new Promise((resolve) => {
+            const dishIndex = this.dishes.findIndex(dish => dish.id === id);
+            this.dishes[dishIndex].deleted_at = new Date();
+            resolve( this.dishes[dishIndex]);
+        });
     }
 }
